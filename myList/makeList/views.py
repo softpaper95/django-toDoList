8f0daa2df8622f65157d661df196cf29352b9cec
softpaper95 <minho100227@likelion.org>
2020-08-15 10:47:58 +0900

    C of CRUD

--- myList/makeList/views.py
+++ myList/makeList/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from makeList.models import Post #객체 Post의 내용(메소드)을 가져옴
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def write(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    #장고는 필드이름(published_date)와 연산자 필터(lte)를 밑줄 2개를 사용해 구분합니다.
+    return render(request, 'write.html', {"posts": posts})

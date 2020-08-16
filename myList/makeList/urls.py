82e33d9691738039b4a30466038f4f77cbf3dcc6
softpaper95 <minho100227@likelion.org>
2020-08-16 14:57:55 +0900

    make post detail page

--- myList/makeList/urls.py
+++ myList/makeList/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('write/', views.write, name='write')
+    path('write/', views.write, name='write'),
+    path('post/<int:pk>/', views.post_detail, name="post_detail"),
 ]
--- myList/makeList/views.py
+++ myList/makeList/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from makeList.models import Post #객체 Post의 내용(메소드)을 가져옴
+
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,7 @@ def write(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     #장고는 필드이름(published_date)와 연산자 필터(lte)를 밑줄 2개를 사용해 구분합니다.
     return render(request, 'write.html', {"posts": posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post':post})
